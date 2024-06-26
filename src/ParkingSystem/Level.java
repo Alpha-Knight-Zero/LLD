a175c492f9b09f47c727e54af5e82abe690d5577
Pushkal Pandey <pushkal@ibm.com>
2024-06-26 21:00:54 +0530

    parking level created.

---
+++ src/ParkingSystem/Level.java
@@ -0,0 +1,48 @@
+package ParkingSystem;
+
+import ParkingSystem.VehicleType.Vehicle;
+import ParkingSystem.VehicleType.VehicleType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Level {
+    private final int floor;
+    private final List<ParkingSpot> parkingSpots;
+
+    public Level(int floor, int numSpots) {
+        this.floor = floor;
+        parkingSpots = new ArrayList<>(numSpots);
+        for (int i = 0; i < numSpots; i++) {
+            VehicleType type= i%4!=0 ? VehicleType.MOTORCYCLE : VehicleType.CAR;
+            parkingSpots.add(ParkingSpot.initParkingSpot(10000+133424*i*i,type));
+        }
+    }
+
+    public synchronized boolean parkVehicle(Vehicle vehicle) {
+        for (ParkingSpot spot : parkingSpots) {
+            if (spot.isAvailable() && spot.getVehicleType() == vehicle.getType()) {
+                spot.parkVehicle(vehicle);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public synchronized boolean removeVehicle(Vehicle vehicle) {
+        for (ParkingSpot spot : parkingSpots) {
+            if (!spot.isAvailable() && spot.getParkedVehicle().equals(vehicle)) {
+                spot.removeVehicle();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void displayAvailability() {
+        System.out.println("Level " + floor + " Availability:");
+        for (ParkingSpot spot : parkingSpots) {
+            System.out.println("Spot " + spot.getSpotNumber() + ": " + (spot.isAvailable() ? "Available" : "Occupied"));
+        }
+    }
+}
