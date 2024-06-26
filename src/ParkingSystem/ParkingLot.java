089ae24b5644c34388f670b3732a8ea6a438ab8d
Pushkal Pandey <pushkal@ibm.com>
2024-06-26 21:03:25 +0530

    parking lot created.

--- src/ParkingSystem/ParkingLot.java
+++ src/ParkingSystem/ParkingLot.java
@@ -1,4 +1,50 @@
 package ParkingSystem;
 
+import ParkingSystem.VehicleType.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot {
+    private static ParkingLot instance;
+    private final List<Level> levels;
+
+    private ParkingLot() {
+        levels = new ArrayList<>();
+    }
+
+    public static synchronized ParkingLot getInstance() {
+        if (instance == null) {
+            instance = new ParkingLot();
+        }
+        return instance;
+    }
+
+    public void addLevel(Level level) {
+        levels.add(level);
+    }
+
+    public boolean parkVehicle(Vehicle vehicle) {
+        for (Level level : levels) {
+            if (level.parkVehicle(vehicle)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean removeVehicle(Vehicle vehicle) {
+        for (Level level : levels) {
+            if (level.removeVehicle(vehicle)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void displayAvailability() {
+        for (Level level : levels) {
+            level.displayAvailability();
+        }
+    }
 }
